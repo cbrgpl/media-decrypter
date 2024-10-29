bd3c666a4bde221fa67acccaf8a6c5a4f72885bc
cbrgpl <cybirgpl@gmail.com>
2024-10-29 19:51:39 +0700

    chore: create separate manifest.json;

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
+import manifest from './manifest.json';
+
 export default defineConfig({
   base: '/media-decrypter',
   build: {
@@ -17,31 +19,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.png'],
-      manifest: {
-        id: '/media-decrypter/',
-        start_url: '/media-decrypter/',
-        scope: '/media-decrypter/',
-        name: 'Decrypter app for personal media',
-        short_name: 'Decrypter',
-        description:
-          'A decrypted/encrypter application to safelly share your media file through messangers. If your messanger accound will be hacked nobody will able to view you media file(sent in chat) because they will be encrypted. For viewing files you must download them locally and decrypt using app. Decrypted files are not stored in filesystem.',
-        display: 'standalone',
-        background_color: '#141414',
-        theme_color: '#E01541',
-        orientation: 'portrait-primary',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
+      manifest: manifest as any,
     }),
   ],
 
